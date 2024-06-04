b0cce766eaec601e39a36754f74742ca888f19d9
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-06-04 16:22:15 +0600

    GH6

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LayoutSide from "../pagesSide/components/Layout/LayoutSide";
 import HomePage from "../pagesSide/Pages/HomePage";
-import LayoutAdmin from "../pagesAdmin/components/Layout/LayoutAdmin";
-
+  
 import AboutPage from "../pagesSide/Pages/AboutPage";
 
 export const router = createBrowserRouter([
@@ -20,11 +19,4 @@ export const router = createBrowserRouter([
       }
     ]
   },
-  {
-    path: '/admin',
-    element: <LayoutAdmin/>,
-    children: [
-     
-    ]
-  }
 ])
